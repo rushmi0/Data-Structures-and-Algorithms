c059a4d533567dd693a3eb33e876c16298670796
rushmi0 <ph.watcharapol_st@tni.ac.th>
2023-10-22 16:03:13 +0700

    เพิ่มคำ บรรยายเข้าไปในโค้ด

--- src/main/java/structures/LinKedList/Pointer.java
+++ src/main/java/structures/LinKedList/Pointer.java
@@ -6,22 +6,28 @@ public class Pointer {
 
     public static void main(String[] args) {
 
-
+        // สร้าง HashMap ชื่อ map1 และ map2
         HashMap<String, Integer> map1 = new HashMap<>();
         HashMap<String, Integer> map2 = new HashMap<>();
 
-        map1.put("value", 2);
+        // เพิ่มค่า "value" ด้วยค่า 11 เข้าใน map1
+        map1.put("value", 11);
+
+        // กำหนดให้ map2 เป็นอ้างอิงถึง map1
         map2 = map1;
 
+        // แสดงค่าทั้งหมดใน map1
         System.out.println(map1);
 
-        // display real memory address for map1
-        System.out.println(System.identityHashCode(map1));
+        // หาที่อยู่หน่วยความจำของ map1 โดยใช้ System.identityHashCode
+        Integer addr1 = System.identityHashCode(map1);
+        System.out.println(addr1);
 
+        // แสดงค่าทั้งหมดใน map2 (ซึ่งก็คือ map1)
         System.out.println(map2);
 
-        // display real memory address for map2
-        System.out.println(System.identityHashCode(map2));
+        // หาที่อยู่หน่วยความจำของ map2 (ซึ่งก็คือ map1) โดยใช้ System.identityHashCode
+        Integer addr2 = System.identityHashCode(map2);
+        System.out.println(addr2);
     }
-
 }
