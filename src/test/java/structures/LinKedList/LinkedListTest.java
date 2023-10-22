ac359ddad74373371c19dd575bf4a06b199fb466
rushmi0 <ph.watcharapol_st@tni.ac.th>
2023-10-23 00:50:56 +0700

    ติดตั้ง JUnit Test 5

---
+++ src/test/java/structures/LinKedList/LinkedListTest.java
@@ -0,0 +1,14 @@
+package structures.LinKedList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
